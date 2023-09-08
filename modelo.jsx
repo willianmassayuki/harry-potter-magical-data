be7a24243a51d1107bd3924c16588791090f181f
Willian Sato <willianmassayukisato@gmail.com>
2023-09-07 23:47:19 -0300

    Dev Notes

---
+++ modelo.jsx
@@ -0,0 +1,52 @@
+0: 
+  actor:"Daniel Radcliffe"
+  alive:true
+  alternate_actors: Array(0)
+    length:0
+    [[Prototype]]:Array(0)
+  alternate_names:Array(2)
+    0:"The Boy Who Lived"
+    1:"The Chosen One"
+    length:2
+    [[Prototype]]:Array(0)
+  ancestry:"half-blood"
+  dateOfBirth:"31-07-1980"
+  eyeColour:"green"
+  gender:"male"
+  hairColour:"black"
+  hogwartsStaff:false
+  hogwartsStudent:true
+  house:"Gryffindor"
+  id:"9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8"
+  image:"https://ik.imagekit.io/hpapi/harry.jpg"
+  name:"Harry Potter"
+  patronus:"stag"
+  species:"human"
+  wand:
+    core:"phoenix feather"
+    length:11
+    wood:"holly"
+    [[Prototype]]: Object
+  wizard:true
+  yearOfBirth:1980
+
+ // Home
+ /* 
+  Cores escuras
+  Castelo de Hogwarts
+  Música tema de fundo
+  Texto "The Wizard World" ou "Harry Potter" aparecendo com animaçãos
+ */   
+
+ // Card
+  image:"https://ik.imagekit.io/hpapi/harry.jpg"
+  name:"Harry Potter"
+  house:"Gryffindor"
+  yearOfBirth:1980
+
+// Página descritiva
+/* 
+  Duas colunas
+  Imagem com animação
+
+*/
