b9bab968ef59fa7575c82726b0eab1ac0f593170
Willian Sato <willianmassayukisato@gmail.com>
2023-09-27 23:58:27 -0300

    Footer

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import "../styles/components/body.scss";
+
+const Body = () => {
+  return (
+    <div>
+      <h1>The Wizard World</h1>
+      <input type="text"></input>
+      <input type="submit" value="Pesquisar" />
+    </div>
+  );
+};
+
+export default Body;
