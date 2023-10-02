d00afb62ce8b508145597407167441933c1b1584
Willian Sato <willianmassayukisato@gmail.com>
2023-10-01 21:29:15 -0300

    Redesign

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,8 +4,8 @@ const Body = () => {
   return (
     <div>
       <h1>The Wizard World</h1>
-      <input type="text"></input>
-      <input type="submit" value="Pesquisar" />
+      {/* <input type="text"></input>
+      <input type="submit" value="Pesquisar" /> */}
     </div>
   );
 };
